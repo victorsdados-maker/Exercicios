1c3be42c663c9b70112c5f98b1d3bbc60bb5344e
victorsdados-maker <victor.s.dados@gmail.com>
2026-03-17 19:05:24 -0300

    if,else e operador de resto.

--- impar_par.py
+++ impar_par.py
@@ -4,7 +4,7 @@
 # A solução mais simples que consegui é a seguinte:
 
 numero = int(input("Digite um número: "))
-if numero % 2 == 0:
+if numero % 2 == 0: 
     print("Par")   
 else:
     print("Ímpar") 
